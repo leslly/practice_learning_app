28bc9eead5a177e9a35dbae17ace07b4811e17a1
joshuauzor <joshuauzor10@gmail.com>
2023-04-14 14:47:01 +0100

    push

--- lib/course/course_info.dart
+++ lib/course/course_info.dart
@@ -19,52 +19,49 @@ class _CourseInfoState extends State<CourseInfo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Center(
+            child: Container(
+              height: 40,
+              width: 40,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: GlobalColors.borderGrey,
+                ),
+              ),
+              child: const Icon(
+                CupertinoIcons.back,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+        title: Text(
+          widget.course.title,
+          style: TextStyle(
+            color: GlobalColors.bigTextColorBlack,
+            fontSize: 24,
+            fontFamily: 'assets/blush_fonts/Rubik/Rubik-Medium',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: GlobalColors.buttonColorwhite,
+        elevation: 0,
+      ),
       body: SingleChildScrollView(
         child: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.only(
-                left: 20, right: 16, top: 24, bottom: 42),
+            padding:
+                const EdgeInsets.only(left: 20, right: 16, top: 24, bottom: 42),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 16, right: 130),
-                      child: InkWell(
-                        onTap: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (
-                              context) => HomePage()));
-                        },
-                        child: Container(
-                          height: 40,
-                          width: 40,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              color: GlobalColors.borderGrey,
-                            ),
-                          ),
-                          child: const Icon(
-                              CupertinoIcons.back
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    Text(
-                      widget.course.title,
-                      style: TextStyle(
-                        color: GlobalColors.bigTextColorBlack,
-                        fontSize: 24,
-                        fontFamily: 'assets/blush_fonts/Rubik/Rubik-Medium.ttf',
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
                 const SizedBox(height: 16),
                 Container(
                   height: 257,
@@ -87,8 +84,7 @@ class _CourseInfoState extends State<CourseInfo> {
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
                       fontFamily: 'assets/blush_fonts/Rubik/Rubik-Medium.ttf',
-                      color: GlobalColors.bigTextColorBlack
-                  ),
+                      color: GlobalColors.bigTextColorBlack),
                 ),
                 SizedBox(height: 10),
                 Text(
@@ -96,8 +92,7 @@ class _CourseInfoState extends State<CourseInfo> {
                   style: TextStyle(
                       fontSize: 14,
                       color: GlobalColors.bigTextColorBlack,
-                      fontFamily: 'assets/blush_fonts/Rubik/Rubik-Regular.ttf'
-                  ),
+                      fontFamily: 'assets/blush_fonts/Rubik/Rubik-Regular.ttf'),
                   overflow: TextOverflow.fade,
                   maxLines: 5,
                 ),
@@ -121,16 +116,18 @@ class _CourseInfoState extends State<CourseInfo> {
                 ),
                 Padding(
                   padding: const EdgeInsets.only(left: 30, right: 17, top: 53),
-
                   child: GlobalButton(
                     colorOfText: GlobalColors.whiteTextColor,
                     colorOfButton: GlobalColors.buttonColorOrange,
                     nextPage: () {
                       //Provider.of<CartModel>(context, listen: false).addItemToCart(index);
-                      Navigator.push(context, MaterialPageRoute(builder: (
-                          context) => CourseSaved()));
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => CourseSaved()));
                     },
-                    text: 'Add to cart',),
+                    text: 'Add to cart',
+                  ),
                 ),
               ],
             ),
@@ -138,6 +135,5 @@ class _CourseInfoState extends State<CourseInfo> {
         ),
       ),
     );
-
   }
 }
